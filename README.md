94f3028f211e2771680ec67acb1e7b870dc6ada6
Cesar Ramirez <cesar@cesar-desktop.localdomain>
2012-06-17 03:30:34 -0500

    Script adg.pl added

--- README.md
+++ README.md
@@ -2,3 +2,35 @@ ADG-Android_Drawable_Generator
 ==============================
 
 This is a tool that allows for the automatic creation of drawables for the different screen densities in the Android  projects.  
+
+Dependencies:
+-Perl
+-ImageMagick
+
+The usage is fairly simple, you call the script and pass as the parameters the image and the attributes 
+for width and heigh of the resulting image for the default size. This image will be a baseline for 
+creating the different sizes, more details can be found here: 
+http://developer.android.com/guide/practices/screens_support.html#DesigningResources.
+
+The image, width and height are mandatory but you can also specify an output directory and a name for 
+the resulting image. By the default the script will place the output in thr current directory and the 
+name will be tha same as the input image. 
+
+For making your life easier, the images are placed in their respective folder(ldi, mdpi, hdpi, 
+xldpi...). I will add more attributes and more options in the future, but for now the most important 
+function is completed.
+
+Example:
+
+#This command will generate the four screen densities for icon.png with 24x24 as base size(mdpi). The 
+output will be placed in the current directory, each file in a separate folder.
+./adg.pl icon.png 24 24
+
+#This command will generate the images but the output will be placed in the res/ folder of the android 
+project. This way you dont have to copy-paste anything.
+./adg.pl icon.png 24 24 --output ~/path/to/project/res
+
+#With the file option you can specify the name of the output image.
+./adg.pl icon.png 24 24 --file click.png
+
+#Remember you can combine both options.
---
+++ adg.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Image::Magick;
+use File::Path qw(make_path remove_tree);
+use Getopt::Long;
+use Cwd;
+
+sub resizeImage{
+  my $width = 0;
+  my $height = 0;
+  my $filename = $_[0];
+  my $subDir = $_[1];
+  my $newName = $_[4];
+  my $outputDirectory = $_[5];
+  if($subDir eq 'drawable'){
+    print "Default size is "; $width = $_[2]; $height = $_[3]; }
+  elsif($subDir eq 'drawable-ldpi')
+    { print "LDPI size is "; $width = $_[2] * 0.75; $height = $_[3] * 0.75;}
+  elsif($subDir eq 'drawable-mdpi')
+    { print "MDPI size is "; $width = $_[2]; $height = $_[3];} 
+  elsif($subDir eq 'drawable-hdpi')
+    { print "HDPI size is "; $width = $_[2] * 1.5; $height = $_[3] * 1.5;} 
+  elsif($subDir eq 'drawable-xhdpi')
+    { print "XHDPI size is "; $width = $_[2] * 2; $height = $_[3]  * 2;}
+  else
+    { print "No correct argument"; return;}
+
+  print "$width"."x"."$height\n";
+
+
+  my $image = Image::Magick->new;
+
+  print "Opening image $filename\n";
+  $image->read($filename);   
+  if($newName ne ''){
+    $filename = $newName;
+  }  
+  $image->Set( Gravity => 'Center' );
+  $image->Set(alpha=>'On');
+  $image->Resize( geometry => "$width"."x"."$height" );
+  #$image->Extent( geometry => "$width"."x"."$height" );
+  print "Saving file to $outputDirectory/$subDir/$filename\n";
+  $image->Write( "$outputDirectory/$subDir/$filename" );
+}
+
+##MAIN
+
+if ( @ARGV == 0 )
+{
+  print "No arguments passed\n";
+  print "Parameters required: [image] [default width] [default height]\n";
+  exit;
+}
+
+print "Starting script\n";
+my $filePath = $ARGV[0];
+my $defaultWidth = $ARGV[1];
+my $defaultHeight = $ARGV[2];
+
+my $outputFileName = '';
+my $outputDirectory = '';
+
+my $result = GetOptions ("file:s" => \$outputFileName, # numeric
+			 "output:s"   => \$outputDirectory);# string
+
+if($outputDirectory =~ m/\/$/){
+  chop($outputDirectory);
+  if($outputDirectory =~ m/\/$/){
+    print "Output directory is not well formatted\n";
+    exit;
+  }
+}
+
+if($outputDirectory eq ''){
+  $outputDirectory = cwd;
+}
+
+print "Creating directories\n";
+make_path("$outputDirectory/drawable", "$outputDirectory/drawable-ldpi", "$outputDirectory/drawable-mdpi", "$outputDirectory/drawable-hdpi", "$outputDirectory/drawable-xhdpi");
+
+resizeImage($filePath, 'drawable', $defaultWidth, $defaultHeight, $outputFileName, $outputDirectory);
+resizeImage($filePath, 'drawable-ldpi', $defaultWidth, $defaultHeight, $outputFileName, $outputDirectory);
+resizeImage($filePath, 'drawable-mdpi', $defaultWidth, $defaultHeight, $outputFileName, $outputDirectory);
+resizeImage($filePath, 'drawable-hdpi', $defaultWidth, $defaultHeight, $outputFileName, $outputDirectory);
+resizeImage($filePath, 'drawable-xhdpi', $defaultWidth, $defaultHeight, $outputFileName, $outputDirectory);
+
